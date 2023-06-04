310ab5989ab969ce20e0f7c9b6d8dda25dfbd02e
Tomás Sarmiento <t.sarmiento01@ufromail.cl>
2023-06-04 00:11:48 -0400

    feat: Add utility functions for calculating days left, progress percentage, and image validity check

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,16 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
-import "./styles/globals.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
 
-// This is the chain your dApp will work on.
-// Change this to the chain your app is built for.
-// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+import App from './App';
 
-const container = document.getElementById("root");
-const root = createRoot(container);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const SEPOLIA_CHAIN_ID = 11155111
 root.render(
-  <React.StrictMode>
-    <ThirdwebProvider activeChain={activeChain}>
-      <App />
-    </ThirdwebProvider>
-  </React.StrictMode>
-);
+    <ThirdwebProvider desiredChainId={ChainId.SEPOLIA_CHAIN_ID}> 
+      <Router>
+          <App />
+      </Router>
+    </ThirdwebProvider> 
+  )
