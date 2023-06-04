777d0cbb5c4c844b2eef4fd16c68dee84ed87f74
Tomás Sarmiento <t.sarmiento01@ufromail.cl>
2023-06-04 00:56:04 -0400

    Complete client project structure and file organization so as to now focus on component development

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,12 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
 
 import App from './App';
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const SEPOLIA_CHAIN_ID = 11155111
 root.render(
-    <ThirdwebProvider desiredChainId={ChainId.SEPOLIA_CHAIN_ID}> 
+    <ThirdwebProvider desiredChainId={SEPOLIA_CHAIN_ID}> 
       <Router>
           <App />
       </Router>
