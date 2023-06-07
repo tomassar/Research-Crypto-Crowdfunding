300ad0c67619bebb63eb38972a92569cac8964f1
Tomás Sarmiento <t.sarmiento01@ufromail.cl>
2023-06-07 13:40:00 -0400

    Add create a campaign and profile button at top right, no functionality implemented yet

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-const CustomButton = () => {
+const CustomButton = ({btnType, title, handleClick, styles}) => {
   return (
-    <div>CustomButton</div>
+    <button 
+      type={btnType}
+      className={`font-epilogue font-semibold text-[15px] leading-[26px] min-h-[52px] px-4 rounded-[4px] ${styles}`}
+      onClick={handleClick}
+    >
+      {title}
+    </button>
   )
 }
 
--- client/src/components/Rightbar.jsx
+++ client/src/components/Rightbar.jsx
@@ -1,10 +1,31 @@
 import React, {useState} from 'react'
 import Icon from './Icon'
+import {Link, useNavigate} from 'react-router-dom'
+
+import CustomButton from './CustomButton'
+import {profile} from '../assets';
 
 const Rightbar = () => {
-
+  const navigate = useNavigate();
+  const address = '0xdsadsa'
   return (
     <div className="flex justify-between items-center flex-col sticky top-5 h-[90vh]">
+      <div className="sm:flex hidden flex-row justify-end gap-4">
+        <CustomButton
+          btnType="button"
+          title={address ? "Create a campaign" : "Connect"}
+          styles={address ? 'bg-[#8fccae90]' : 'bg-[#8c6dfd]'}
+          handleClick={() => {
+            if(address) navigate('create-campaign')
+            else 'connect()'
+          }}
+        />
+        <Link to="/profile">
+          <div className="w-[52px] h-[52px] rounded-[10px] bg-[#1a1c1b20] flex justify-center items-center cursor-pointer">
+            <img src={profile} alt="user" className="w-[60%] h-[60%] object-contain" />
+          </div>
+        </Link>
+      </div>
       <div className="flex-1 flex flex-col justify-between items-center bg-[#1a1c1b20] w-[200px] py-4 mt-12">
         <div className="flex flex-col justify-center items-center gap-3"> 
           Topic list
